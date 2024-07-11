ea2ee26a69e66d6b8a792d6de8a0cbe90f05043b
deshinta <deshintaaulia065@gmail.com>
2024-07-11 08:41:26 +0700

    feat: add middleware errors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const errors = require("./middleware/errors");
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(errors());
 
 app.listen(port, () => console.log(`listen on port ${port}...`));
--- middleware/errors.js
+++ middleware/errors.js
@@ -1,1 +1,9 @@
-module.exports = function errors(error, req, res, next) {};
+module.exports = function errors(error, req, res, next) {
+  let status = 500;
+  let message = "Internal Server Error";
+  if (error.name === "Bad Request") {
+    status = 404;
+    message = error.message;
+  }
+  res.status(status).json({ message });
+};
