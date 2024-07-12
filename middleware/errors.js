9f76137e206139b28c993c85e4a726a1925a7992
deshinta <deshintaaulia065@gmail.com>
2024-07-13 06:24:22 +0700

    feat: add api doct with swagger

--- middleware/errors.js
+++ middleware/errors.js
@@ -1,7 +1,7 @@
 module.exports = function errors(error, req, res, next) {
   let status = 500;
   let message = "Internal Server Error";
-  console.log(error, "<< error di function error");
+  // console.log(error, "<< error di function error");
   if (error.name === "Bad Request") {
     status = 400;
     message = error.message;
