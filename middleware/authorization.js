8d26363e4f172afd934cf55a4ea4fa24e7ed2b5e
deshinta <deshintaaulia065@gmail.com>
2024-07-12 14:39:42 +0700

    feat: add feature check status pinalty

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,17 +2,32 @@ const { Member, MembersBook } = require("../models/");
 module.exports = async function authorization(req, res, next) {
   try {
     // check jumlah hari setelah pengembalian buku pinalti
-    // update status member book ke borrowed
-    // check lagi apakah masih ada yang terkena pinalti
     let checkStatus = await MembersBook.findOne({
       where: { MemberId: req.user.id, status: "penalized" },
     });
-    if (!checkStatus) {
+    // hari ini dikurangi hari waktu status berubah apakah lebih dari 3 hari
+    if (
+      checkStatus &&
+      new Date().getDate() - checkStatus.updatedAt.getDate() > 3
+    ) {
+      // update status member book ke borrowed
+      let check = await MembersBook.update(
+        { status: "returned" },
+        { where: { id: checkStatus.id } }
+      );
+    }
+    // check lagi apakah masih ada yang terkena pinalti
+    let checkPinalty = await MembersBook.findAll({
+      where: { MemberId: req.user.id, status: "penalized" },
+    });
+
+    if (!checkPinalty.length) {
       next();
     } else {
       throw {
         name: "Forbidden",
-        message: "Member is currently being penalized",
+        message:
+          "Member is currently being penalized. Member with penalty cannot able to borrow the book for 3 days.",
       };
     }
   } catch (error) {
